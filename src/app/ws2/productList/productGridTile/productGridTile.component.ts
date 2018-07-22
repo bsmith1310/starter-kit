9bf45d292532db69714753216c141955c1191cf7
Brent Smith <brent.smith@mrrsoft.com>
2018-07-22 13:29:31 -0500

    Improving product listing in WS 2. Adding compact responsive grid example to WS 3.

--- src/app/ws2/productList/productGridTile/productGridTile.component.ts
+++ src/app/ws2/productList/productGridTile/productGridTile.component.ts
@@ -1,21 +1,17 @@
-// import { Component, Input, OnInit } from '@angular/core';
-//
-// import { environment } from '../../../../environments/environment';
-//
-// @Component({
-//   selector: 'app-product-grid-tile',
-//   templateUrl: './productGridTile.component.html',
-//   styleUrls: ['./productGridTile.component.scss']
-// })
-// export class ProductGridTileComponent implements OnInit {
-//
-//   @Input()
-//   product: any;
-//
-//   version: string = environment.version;
-//
-//   constructor() { }
-//
-//   ngOnInit() { }
-//
-// }
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-grid-tile',
+  templateUrl: './productGridTile.component.html',
+  styleUrls: ['./productGridTile.component.scss']
+})
+export class ProductGridTileComponent implements OnInit {
+
+  @Input()
+  product: any;
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
--- src/app/ws2/ws2.module.ts
+++ src/app/ws2/ws2.module.ts
@@ -5,6 +5,7 @@ import { TranslateModule } from '@ngx-translate/core';
 import { Ws2RoutingModule } from './ws2-routing.module';
 import { Ws2Component } from './ws2.component';
 
+import { ProductGridTileComponent } from './productList/productGridTile/productGridTile.component';
 import { ProductListComponent } from './productList/productList.component';
 
 import { HeaderItemsService } from './headerItems.service';
@@ -18,6 +19,7 @@ import { ProductService } from './productList/product.service';
   ],
   declarations: [
     Ws2Component,
+    ProductGridTileComponent,
     ProductListComponent
   ],
   providers: [
