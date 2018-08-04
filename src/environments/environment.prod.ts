b59ad784c13fdbc34c78fc6a9c25a4565cb75550
Brent Smith <brent.smith@mrrsoft.com>
2018-08-03 22:39:09 -0500

    Fixing build for production configuration.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,24 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'https://api.chucknorris.io',
+  serverUrl: '/api',
   defaultLanguage: 'en-US',
   supportedLanguages: [
     'en-US',
     'fr-FR'
-  ]
+  ],
+  znodeApiUrl: 'http://localhost:44762',
+  znodeWebStoreApiUrl: 'http://localhost:3288',
+  znodeWebStoreApiResources: [ // all resources fetched from Znode are assumed to be on API (ex: 44762),
+    // unless on this list and assumed to be on WebStore (ex: 3288)
+    'angularpoc/bannerslider',
+    'headeritems'
+  ],
+  znodeWebStoreApiCacheablePosts: [ // all GET requests to Znode are assumed to be cacheable,
+    // POSTs can also be cached by adding to this list
+    'search/fulltextsearch'
+  ],
+  storeSettings: {
+    catelogId: 3
+  }
 };
